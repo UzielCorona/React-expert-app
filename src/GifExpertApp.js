40cd76cf562bda53f236f789db53bb07240f78f6
CaydeSix <mcorona@bluepeople.com>
2022-02-28 20:35:09 -0600

    siuuuuux2

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -8,7 +8,7 @@ export const GifExpertApp = () => {
 
   return (
     <>
-        <h2>GifExpertApp</h2>  
+        <h2>Prueba</h2>  
         <AddCategory setCategories={setCategories}/> 
         <hr></hr>
         <ol>
